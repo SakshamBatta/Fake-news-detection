1008251920417bbf4b2eff0feb25a5f469934b77
Saksham Batta <sakshambatta20@gmail.com>
2023-12-01 00:34:43 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask,escape,request,render_template
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+# initialise a Tfidvectorizer
+vector = TfidfVectorizer(stop_words = 'english',max_df=0.7)
+
+vector = pickle.load(open("vectorizer.pkl",'rb'))
+
+model = pickle.load(open("finalised_model.pkl",'rb'))
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return render_template("index.html")
+
+@app.route("/prediction",methods=['GET','POST'])
+def prediction():
+  if request.method =="POST":
+    news = request.form['news']
+    
+    predict = model.predict(vector.transform([news]))
+    print(predict)
+
+    return render_template("prediction.html",prediction_text="News headline is -> {}".format(predict))
+
+  else:
+    return render_template("prediction.html")
+
+@app.route("/about")
+def about():
+  return render_template("aboutUs.html")
+
+
+
+
+if __name__ == "__main__":
+  app.run()
